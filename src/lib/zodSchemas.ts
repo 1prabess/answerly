fd941eb375e20133312e9da08bb32a2ff61f9660
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-09-26 15:12:03 +0545

    Add question schema and API response type
    Introduces createQuestionSchema for validating question creation input and ApiResponse generic interface for API responses. Also updates RegisterSchema password minimum length and adds LoginSchema and its type.

--- src/lib/zodSchemas.ts
+++ src/lib/zodSchemas.ts
@@ -1,10 +1,18 @@
 import { z } from "zod";
 
-// Zod for registering a user
+// Register account
 export const RegisterSchema = z.object({
   username: z.string().min(4, "Username must be at least 4 characters."),
   email: z.email(),
-  password: z.string().min(6, "Password must be at least 6 characters long."),
+  password: z.string().min(8, "Password must be at least 8 characters long."),
 });
 
 export type RegisterType = z.infer<typeof RegisterSchema>;
+
+// Login to account
+export const LoginSchema = z.object({
+  email: z.email(),
+  password: z.string(),
+});
+
+export type LoginType = z.infer<typeof LoginSchema>;

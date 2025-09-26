4e914b4a23597076146af94a1bf3b39ff391302a
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-09-26 15:10:14 +0545

    Update page.tsx

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,8 +2,10 @@ import LoginForm from "./_components/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div>
-      <LoginForm />
+    <div className="flex flex-col mt-20 items-center justify-center gap-6 p-2 md:p-10">
+      <div className="w-full max-w-md flex flex-col gap-6 mx-auto">
+        <LoginForm />
+      </div>
     </div>
   );
 };

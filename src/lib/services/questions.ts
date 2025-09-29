e9967ad055f0ad4670f9353aab1c8cc1e5b35f01
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-09-29 08:47:25 +0545

    Create questions services

---
+++ src/lib/services/questions.ts
@@ -0,0 +1,30 @@
+import { FeedQuestion } from "@/types/question";
+import axios from "axios";
+import { CreateQuestionType } from "../zod/questionSchema";
+
+export const getQuestions = async () => {
+  const response = await axios.get("/api/questions");
+
+  const questions: FeedQuestion[] = response.data.data;
+
+  return questions;
+};
+
+export const voteQuestion = async (
+  questionId: string,
+  userId: string,
+  type: "UP" | "DOWN"
+) => {
+  const response = await axios.post(`/api/questions/${questionId}`, {
+    userId,
+    type,
+  });
+
+  return response.data.data;
+};
+
+export const createQuestion = async (payload: CreateQuestionType) => {
+  const response = await axios.post(`/api/questions`, payload);
+
+  return response.data.data;
+};

f6413ca1d56d7fc790df4c600893dcc901ddc903
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-09-29 07:22:25 +0545

    Update page.tsx

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,8 +2,8 @@ import RegisterForm from "./_components/RegisterForm";
 
 const RegisterPage = () => {
   return (
-    <div className="flex flex-col items-center mt-20 justify-center gap-6 p-2 md:p-10">
-      <div className="w-full max-w-md flex flex-col gap-6 mx-auto">
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="w-full max-w-md p-4">
         <RegisterForm />
       </div>
     </div>

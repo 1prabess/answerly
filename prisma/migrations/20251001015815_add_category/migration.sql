2811fbb7e72056091a14d3ecdd023cd248f86dbc
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-10-03 08:10:31 +0545

    Add Tag, Category, and Follow tables with relations
    Introduces new tables for Tag, Category, and Follow entities. Adds unique constraints to tag and category names, establishes relations between tags and categories, and sets up a many-to-many relationship between questions and tags. Also creates the Follow table to support user following functionality.

---
+++ prisma/migrations/20251001015815_add_category/migration.sql
@@ -0,0 +1,30 @@
+-- AlterTable
+ALTER TABLE "Tag" ADD COLUMN     "categoryId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_QuestionToTag" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_QuestionToTag_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_QuestionToTag_B_index" ON "_QuestionToTag"("B");
+
+-- AddForeignKey
+ALTER TABLE "Tag" ADD CONSTRAINT "Tag_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_QuestionToTag" ADD CONSTRAINT "_QuestionToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Question"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_QuestionToTag" ADD CONSTRAINT "_QuestionToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20251001103254_add_follow/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Follow" (
+    "id" TEXT NOT NULL,
+    "followerId" TEXT NOT NULL,
+    "followingId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Follow_pkey" PRIMARY KEY ("id")
+);

48f2493c1c2ef5f9accdb9b481901222c7c4c4e5
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-10-04 18:25:35 +0545

    Add username field as additional field for better auth
    Introduces an optional, unique 'username' field to the user schema in the auth configuration. Updates the auth client to use the inferAdditionalFields plugin for type inference based on the new field.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, User } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 import { config } from "@/config";
@@ -20,5 +20,16 @@ export const auth = betterAuth({
       clientSecret: config.GITHUB_CLIENT_SECRET as string,
     },
   },
+
+  user: {
+    additionalFields: {
+      username: {
+        type: "string",
+        required: false,
+        unique: true,
+      },
+    },
+  },
+
   trustedOrigins: ["http://localhost:3000"],
 });

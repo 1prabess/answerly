98a4728ccd910b47c2c20a14666e793fd3709f77
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-10-05 20:05:37 +0545

    Add Comment model to Prisma schema
    Introduces a new Comment model with relations to User and Question. Updates the Prisma schema and creates a migration to add the comment table and foreign key constraints.

---
+++ prisma/migrations/20251005015848_add_comment_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "comment" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "questionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comment" ADD CONSTRAINT "comment_questionId_fkey" FOREIGN KEY ("questionId") REFERENCES "Question"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

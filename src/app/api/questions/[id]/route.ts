5c4d23125c7ab128f7f8f478ba16745a5e301893
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-10-05 20:06:00 +0545

    Add GET endpoint for question details
    Implements a GET handler to fetch question details by ID, including author, tags, vote counts, score, and the current user's vote. Returns appropriate error responses for missing or invalid question IDs.

--- src/app/api/questions/[id]/route.ts
+++ src/app/api/questions/[id]/route.ts
@@ -1,9 +1,97 @@
+import { Question } from "@/generated/prisma";
+import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
-import { ApiResponse } from "@/types/api";
+import {
+  ApiResponse,
+  QuestionDetails,
+  QuestionWithAuthorAndTags,
+} from "@/types/api";
 import { Vote } from "@/types/vote";
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-// Vote questions
+// Get question
+export const GET = async (
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  try {
+    const { id: questionId } = await params;
+    const session = await auth.api.getSession({ headers: await headers() });
+    const userId = session?.user?.id;
+
+    if (!questionId) {
+      return NextResponse.json<ApiResponse<never>>(
+        {
+          success: false,
+          error: "QuestionId is required!",
+        },
+        { status: 400 }
+      );
+    }
+
+    const question = await prisma.question.findUnique({
+      where: {
+        id: questionId,
+      },
+      include: {
+        author: {
+          select: {
+            image: true,
+            name: true,
+            username: true,
+          },
+        },
+        tags: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    if (!question) {
+      return NextResponse.json<ApiResponse<never>>(
+        {
+          success: false,
+          error: "Question not found.",
+        },
+        { status: 404 }
+      );
+    }
+
+    const votes: Vote[] = await prisma.vote.findMany({ where: { questionId } });
+
+    const upVotes = votes.filter((v) => v.type === "UP").length;
+    const downVotes = votes.filter((v) => v.type === "DOWN").length;
+    const score = upVotes - downVotes;
+
+    const userVote = userId
+      ? votes.find((v) => v.userId === userId)?.type || null
+      : null;
+
+    const questionDetails = {
+      ...question,
+      upVotes,
+      downVotes,
+      score,
+      userVote,
+    };
+
+    return NextResponse.json<ApiResponse<QuestionDetails>>({
+      success: true,
+      data: questionDetails,
+    });
+  } catch (error) {
+    console.log("Error in getting question:", error);
+    return NextResponse.json<ApiResponse<never>>(
+      { success: false, error: "Internal server error." },
+      { status: 500 }
+    );
+  }
+};
+
+// Vote question
 export const POST = async (
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> }

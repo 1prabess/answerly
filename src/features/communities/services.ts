68228c8f928be7024b8d023dfeb4130756a16b8f
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-11-02 12:58:31 +0545

    Create services for communities

--- src/features/communities/services.ts
+++ src/features/communities/services.ts
@@ -1,6 +1,10 @@
 import { config } from "@/config";
 import { ApiResponse } from "@/types/api";
 import { Community, CommunityDetails } from "@/types/community";
+import {
+  DeleteCommunityMemberPayload,
+  UpdateCommunityMemberPayload,
+} from "./types";
 
 // Creates a community
 export const createCommunity = async (
@@ -65,6 +69,71 @@ export const joinCommunity = async (communityName: string) => {
 
   if (!response.ok || !data.success) {
     throw new Error(data.error || "Failed to join the  community.");
+  }
+
+  return data.data;
+};
+
+// Get community members
+export const getCommunityMembers = async (communityName: string) => {
+  const response = await fetch(
+    `${config.NEXT_PUBLIC_BASE_URL}/api/communities/${communityName}/members`,
+  );
+
+  const data = await response.json();
+
+  if (!response.ok || !data.success) {
+    throw new Error(data.error || "Failed to get community members.");
+  }
+
+  return data.data;
+};
+
+// Update role of a member
+export const updateCommunityMember = async (
+  communityName: string,
+  payload: UpdateCommunityMemberPayload,
+) => {
+  const response = await fetch(
+    `${config.NEXT_PUBLIC_BASE_URL}/api/communities/${communityName}/members`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    },
+  );
+
+  const data = await response.json();
+
+  if (!response.ok || !data.success) {
+    throw new Error(data.error || "Failed to update member role.");
+  }
+
+  return data.data;
+};
+
+// Delete a member from community
+export const deleteCommunityMember = async (
+  communityName: string,
+  payload: DeleteCommunityMemberPayload,
+) => {
+  const response = await fetch(
+    `${config.NEXT_PUBLIC_BASE_URL}/api/communities/${communityName}/members`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    },
+  );
+
+  const data = await response.json();
+
+  if (!response.ok || !data.success) {
+    throw new Error(data.error || "Failed to delete community member.");
   }
 
   return data.data;

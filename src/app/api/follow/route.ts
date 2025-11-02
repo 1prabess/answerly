a40759ba7112674043275ec079d997bf2cace3fb
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-11-02 12:59:53 +0545

    Update route.ts

--- src/app/api/follow/route.ts
+++ src/app/api/follow/route.ts
@@ -3,7 +3,6 @@ import prisma from "@/lib/prisma";
 import { ApiResponse } from "@/types/api";
 import { NextRequest, NextResponse } from "next/server";
 
-// Follow user
 export const POST = async (request: NextRequest) => {
   try {
     const { followerId, followingId } = await request.json();
@@ -11,7 +10,7 @@ export const POST = async (request: NextRequest) => {
     if (followerId === followingId) {
       return NextResponse.json<ApiResponse<never>>(
         { success: false, error: "You cannot follow yourself." },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
@@ -20,25 +19,31 @@ export const POST = async (request: NextRequest) => {
     });
 
     if (existing) {
-      return NextResponse.json<ApiResponse<Follow>>({
-        success: true,
-        data: existing,
+      // toggle: unfollow
+      await prisma.follow.delete({
+        where: { id: existing.id },
       });
+
+      return NextResponse.json<ApiResponse<never>>(
+        { success: true, error: "Unfollowed successfully." },
+        { status: 200 },
+      );
+    } else {
+      // toggle: follow
+      const follow = await prisma.follow.create({
+        data: { followerId, followingId },
+      });
+
+      return NextResponse.json<ApiResponse<Follow>>(
+        { success: true, data: follow },
+        { status: 201 },
+      );
     }
-
-    const follow = await prisma.follow.create({
-      data: { followerId, followingId },
-    });
-
-    return NextResponse.json<ApiResponse<Follow>>(
-      { success: true, data: follow },
-      { status: 201 }
-    );
   } catch (error) {
-    console.log("Error in following the user:", error);
+    console.log("Error in toggling follow:", error);
     return NextResponse.json<ApiResponse<never>>(
       { success: false, error: "Internal server error." },
-      { status: 500 }
+      { status: 500 },
     );
   }
 };
